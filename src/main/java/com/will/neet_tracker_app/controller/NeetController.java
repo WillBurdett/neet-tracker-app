ed2e15d71ad6eed46dea949e9a86b1f63172dc9d
WillBurdett <williamburdett1995@gmail.com>
2025-02-10 20:49:50 +0000

    basic thymeleaf homepage added

--- src/main/java/com/will/neet_tracker_app/controller/NeetController.java
+++ src/main/java/com/will/neet_tracker_app/controller/NeetController.java
@@ -8,14 +8,15 @@ import com.will.neet_tracker_app.service.SubmissionService;
 import com.will.neet_tracker_app.service.UnitService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/neet")
 public class NeetController {
 
@@ -28,6 +29,11 @@ public class NeetController {
     this.problemService = problemService;
     this.submissionService = submissionService;
     this.unitService = unitService;
+  }
+
+  @GetMapping
+  public String viewHomepage(Model model) {
+    return "index";
   }
 
   @GetMapping("/problem")
